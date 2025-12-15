29e42004a41298a7fac4326cbf078ba152def5f2
Shivansh Chaurasiya <shivanshc1203btechds2026@kccitm.edu.in>
2025-12-15 17:47:49 +0530

    Fix Router name collision with Wouter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
+function AppRoutes() {
 
-function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
