ec941f91eda5ecff8c28ec77a72c2d535bbfa6ee
Shivansh Chaurasiya <shivanshc1203btechds2026@kccitm.edu.in>
2025-12-15 17:50:20 +0530

    Temporarily remove routing to fix GitHub Pages 404

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,9 @@
-import { Switch, Route, Router } from "wouter";
-import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
 import Home from "@/pages/home";
-import NotFound from "@/pages/not-found";
-function AppRoutes() {
-
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
 
 function App() {
   return (
@@ -22,13 +11,7 @@ function App() {
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
-          <Router base="/my-portfolio">
-  <Switch>
-    <Route path="/*" component={Home} />
-  </Switch>
-</Router>
-
- 
+          <Home />
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
