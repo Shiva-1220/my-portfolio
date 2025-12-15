88e966c6cfde7f4989a2a7c716998a2aef66289e
Shivansh Chaurasiya <shivanshc1203btechds2026@kccitm.edu.in>
2025-12-15 17:40:19 +0530

    Fix Wouter base path for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -22,7 +22,13 @@ function App() {
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
-          <Router />
+          <Router base="/my-portfolio">
+  <Switch>
+    <Route path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+</Router>
+ 
         </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
